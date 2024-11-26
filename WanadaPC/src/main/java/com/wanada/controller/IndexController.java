4a60497c9c429f139c51caaeeb15fc8c33edfa09
DESKTOP-VLQ0GRC\JSLHRD <jin.harry97@gmail.com>
2024-11-26 11:45:44 +0900

    11/26 - ahn 최종 (search 의 product 부분 제외)

--- WanadaPC/src/main/java/com/wanada/controller/IndexController.java
+++ WanadaPC/src/main/java/com/wanada/controller/IndexController.java
@@ -2,8 +2,6 @@ package com.wanada.controller;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,8 +19,6 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
-	private static final Logger log = LoggerFactory.getLogger("ThymeleafController");
-	
 	@Autowired
 	private IndexService service;
 	
