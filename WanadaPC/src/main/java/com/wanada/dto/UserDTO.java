4a787d44315c0aef1b76edfefd8d04c82df18231
JOYUNAM <dbska7329@naver.com>
2024-12-03 12:44:15 +0900

    no message

--- WanadaPC/src/main/java/com/wanada/dto/UserDTO.java
+++ WanadaPC/src/main/java/com/wanada/dto/UserDTO.java
@@ -1,9 +1,12 @@
 package com.wanada.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
-
+@Getter
+@Setter
 public class UserDTO {
 	private String userEmail;
 	private String userPassword;
